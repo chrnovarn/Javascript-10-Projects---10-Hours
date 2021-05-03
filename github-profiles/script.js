11649e61327676fbf9d6049fcdff5160f192291e
Jose Martinez <parraworld@gmail.com>
2021-05-03 21:28:14 +0100

    github-app ended

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -2,31 +2,68 @@ const APIURL = 'https://api.github.com/users/';
 const main = document.querySelector('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-
-getUser("chrnovarn");
+const ApiToken = "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";
+//getUser("chrnovarn");
 
 async function getUser(user){
-    const resp = await fetch(APIURL+user);
+    const resp = await fetch(
+        APIURL+user, 
+        {headers: new Headers({
+        'Authorization': ApiToken, 
+        })}
+    );
+    const headers = await resp;
     const respData = await resp.json();
-
+    
     createUserCard(respData);
+
+    getRepos(user);
 }
 
+async function getRepos(user){
+    const resp = await fetch(
+        APIURL+user+"/repos",
+        {headers: new Headers({
+            'Authorization': ApiToken, 
+            })}
+    );
+    const repos = await resp.json();
+
+
+    
+    addReposToCard(repos);
+};
+function addReposToCard(repos){
+    console.log(repos);
+    const reposEl = document.getElementById("repos");
+    repos.sort((a,b) => b.stargazers_count - a.stargazers_count)
+        .slice(0,10)
+        .forEach(repo => {
+        const repoEl = document.createElement("a");
+        repoEl.classList.add("repo");
+        repoEl.href = repo.html_url;
+        repoEl.target = "_blank";
+        repoEl.innerText = repo.name;
+        reposEl.appendChild(repoEl);
+    })
+    
+}
 function createUserCard(user){
     
     const cardHTML = `
     <div class="card">
-        <div class="img-container">
+        <div>
             <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
         </div>
         <div class="user-info">
             <h2>${user.name}</h2>
             <p>${user.bio}</p>
             <ul class="info">
-                <li>${user.followers}</li>
-                <li>${user.following}</li>
-                <li>${user.public_repos}</li>
+                <li><strong>${user.followers} Followers</strong></li>
+                <li><strong>${user.following} Following</strong></li>
+                <li><strong>${user.public_repos} Repos</strong></li>
             </ul>
+            <div id="repos"></div>
         </div>
     </div>
     `
